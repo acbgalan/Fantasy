376864738a146f739689d2110f484e794438bd31
Carlos Blanco <cblanco@xpander.es>
2024-09-05 14:58:34 +0200

    Repository added as service in program.cs

--- src/Fantasy.Frontend/Program.cs
+++ src/Fantasy.Frontend/Program.cs
@@ -1,4 +1,5 @@
 using Fantasy.Frontend;
+using Fantasy.Frontend.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -8,5 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7062/") });
+
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
